712a49a52e18c6c50faac2dcd2d34a15a64a6645
Pyro <PyroLovesbuns@gmail.com>
2024-05-06 16:06:16 +0300

    more updated ui v2

--- module/data/domain.mjs
+++ module/data/domain.mjs
@@ -1,25 +1,37 @@
 export default class DaggerHeartDomain extends foundry.abstract.TypeDataModel {
-  static defineSchema() {
-    const fields = foundry.data.fields;
-    const schema = {};
+  static defineSchema () {
+    const fields = foundry.data.fields
+    const schema = {}
 
     schema.domain = new fields.StringField({
       blank: true,
-    });
+    })
 
     schema.domainType = new fields.StringField({
       blank: true,
-    });
+    })
 
     schema.feature = new fields.StringField({
       blank: true,
-    });
+    })
 
-    schema.recall = new fields.NumberField({ initial: 0, integer: true });
-    schema.level = new fields.NumberField({ initial: 0, integer: true });
+    schema.hasTracker = new fields.SchemaField({
+      enabled: new fields.BooleanField({
+        initial: false,
+      }),
+      property: new fields.StringField({
+        initial: '',
+      }),
+      current: new fields.NumberField({
+        initial: 0,
+      }),
+    })
 
-    schema.active = new fields.BooleanField({ initial: false });
+    schema.recall = new fields.NumberField({ initial: 0, integer: true })
+    schema.level = new fields.NumberField({ initial: 0, integer: true })
 
-    return schema;
+    schema.active = new fields.BooleanField({ initial: false })
+
+    return schema
   }
 }
